3ec59ebf13e3a195552e99a61a8c7ca3a4241e9b
lbreede <67719520+lbreede@users.noreply.github.com>
2024-09-11 19:12:54 -0700

    Added documentation

--- src/random_utils.rs
+++ src/random_utils.rs
@@ -1,19 +1,23 @@
 use nalgebra::Vector3;
 use rand::Rng;
 
-pub fn random_vector() -> Vector3<f32> {
-    Vector3::new(random_float(), random_float(), random_float())
-}
-
+/// Generate a random float in the range [0, 1).
 pub fn random_float() -> f32 {
     let mut rng = rand::thread_rng();
     rng.gen()
 }
 
+/// Generate a random vector with each component in the range [0, 1).
+pub fn random_vector() -> Vector3<f32> {
+    Vector3::new(random_float(), random_float(), random_float())
+}
+
+/// Generate a random float in the range [min, max).
 pub fn random_float_range(min: f32, max: f32) -> f32 {
     min + (max - min) * random_float()
 }
 
+/// Generate a random vector with each component in the range [min, max).
 pub fn random_vector_range(min: f32, max: f32) -> Vector3<f32> {
     Vector3::new(
         random_float_range(min, max),
